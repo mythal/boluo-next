52583caaf7a888066865a8b0533772501934bdbf
Tachibana Kiyomi <me@yuru.me>
2021-08-24 02:15:33 +0900

    chore: trailing slash

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
+  trailingSlash: true,
   webpack5: true,
   i18n: {
     locales: ['en', 'ja', 'zh-CN'],
     defaultLocale: 'en',
   },
   webpack: (config) => {
+    // noinspection JSValidateTypes
     config.module.rules.push({
       test: /\.svg$/,
       use: [
@@ -22,6 +24,8 @@ module.exports = {
       ],
     });
 
+    // `react-intl` without parser
+    // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
     config.resolve.alias['@formatjs/icu-messageformat-parser'] = '@formatjs/icu-messageformat-parser/no-parser';
     return config;
   },
