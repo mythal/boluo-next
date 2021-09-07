f236f04516dea176c6062f82cebe05694c164ed1
Tachibana Kiyomi <me@yuru.me>
2021-09-07 11:01:28 +0900

    chore: merge `additional.d.ts` to `env.d.ts`

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,15 @@
+/// <reference types="@emotion/react/types/css-prop" />
+
+// SVGR with Typescript
+// https://github.com/gregberge/svgr/issues/38
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+
+  export { ReactComponent };
+  export default ReactComponent;
+}
+
+
 import '@emotion/react';
 import { Theme as MyTheme } from './styles/theme';
 
