d7611056739f4130381128c3ee7d69755a52b68e
Tachibana Kiyomi <me@yuru.me>
2022-03-18 19:31:43 +0800

    feat(styles): design page styles

--- src/pages/design/[[...params]].tsx
+++ src/pages/design/[[...params]].tsx
@@ -3,6 +3,9 @@ import { FC } from 'react';
 import Link from 'next/link';
 import { tabRouteTable, useDesignRoute } from '../../design/useDesignRoute';
 import Home from '../../design/Home.mdx';
+import { css, Theme } from '@emotion/react';
+import { gray } from '../../styles/utility/color';
+import { SchemeSwitch } from '../../components/SchemeSwitch';
 
 const DesignRoute: FC<{ tab: keyof typeof tabRouteTable }> = ({ tab }) => {
   if (tabRouteTable.hasOwnProperty(tab)) {
@@ -10,6 +13,36 @@ const DesignRoute: FC<{ tab: keyof typeof tabRouteTable }> = ({ tab }) => {
   } else {
     return <Home />;
   }
+};
+
+const styles = {
+  container: css`
+    display: flex;
+  `,
+  sidebar: (theme: Theme) => css`
+    background: ${theme.mode === 'light' ? gray['100'] : gray['900']};
+    padding: 1em 2.5em;
+    height: 100vh;
+    ul {
+      padding: 0;
+      list-style: none;
+      a {
+        display: block;
+        padding: 0.25rem 0;
+
+        &[data-active='true'] {
+          text-decoration: underline;
+        }
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  `,
+  content: css`
+    padding: 1em 2em;
+  `,
 };
 
 const Design: Page = () => {
@@ -24,11 +57,15 @@ const Design: Page = () => {
     );
   });
   return (
-    <div>
-      <div>
+    <div css={styles.container}>
+      <div css={styles.sidebar}>
         <ul>{sidebarItems}</ul>
+
+        <div>
+          <SchemeSwitch />
+        </div>
       </div>
-      <div>
+      <div css={styles.content}>
         <DesignRoute tab={tab} />
       </div>
     </div>
