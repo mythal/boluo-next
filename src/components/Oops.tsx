e8079d51e837847e2dc609c3e6760a4599315fac
Tachibana Kiyomi <me@yuru.me>
2022-03-30 09:52:43 +0800

    feat: new <Oops/>

--- src/components/Oops.tsx
+++ src/components/Oops.tsx
@@ -2,51 +2,21 @@ import { FormattedMessage } from 'react-intl';
 import React, { useRef } from 'react';
 import { css } from '@emotion/react';
 import { useContainerQuery } from '../hooks/useContainerQuery';
-import { RefreshButton } from './RefreshButton';
 import { Text } from './fundamental/Text';
+import { text } from '../styles/utility/typography';
 import { float } from '../styles/utility/layout';
+import { RefreshButton } from './RefreshButton';
 import { m } from '../styles/utility/spacing';
+import { w } from '../styles/utility/sizing';
+import Icon from './fundamental/Icon';
 
 const styles = {
   container: css`
-    background-color: #000;
-    color: #fff;
     padding: 1rem;
     width: 100%;
     height: 100%;
+    overflow: hidden;
   `,
-  information: css`
-    padding-bottom: 0.5em;
-  `,
-  message: css`
-    width: 100%;
-  `,
-  button: css`
-    float: right;
-    font-size: 1rem;
-  `,
-  errorMessage: css`
-    font-family: monospace;
-  `,
-  title: css`
-    padding: 0.25em 0;
-    flex: 0 0 auto;
-    font-weight: bold;
-    [data-width='lg'] & {
-      font-size: 2em;
-    }
-    [data-width='md'] & {
-      font-size: 1.5em;
-    }
-    [data-width='sm'] &,
-    [data-width='xs'] & {
-      font-size: 1.25em;
-    }
-  `,
-  summary: css`
-    font-size: 0.875em;
-  `,
-  details: css``,
 };
 
 export interface Props {
@@ -54,37 +24,79 @@ export interface Props {
   className?: string;
 }
 
-const widthBreakpoints = {
-  xs: 0,
-  sm: 150,
-  md: 300,
-  lg: 600,
+const config = {
+  width: {
+    xs: 0,
+    sm: 150,
+    md: 300,
+  },
+  height: {
+    xs: 0,
+    sm: 100,
+    md: 150,
+  },
 };
 
+function OopsXs({}: Props) {
+  return (
+    <div css={w.full}>
+      <Text>
+        <FormattedMessage defaultMessage="Something going wrong." />{' '}
+        <a href="#" onClick={() => window.location.reload()}>
+          <Icon icon="refresh" css={m.r(1)} />
+          <FormattedMessage defaultMessage="refresh" />
+        </a>
+      </Text>
+    </div>
+  );
+}
+
+function OopsMd({}: Props) {
+  return (
+    <>
+      <RefreshButton css={[float.right, m.b(2), m.l(2)]} text={false} small />
+      <Text css={[text.lg]}>
+        <FormattedMessage defaultMessage="Oops" />
+      </Text>
+      <Text size="small">
+        <FormattedMessage defaultMessage="Something going wrong." />
+      </Text>
+    </>
+  );
+}
+function OopsLg({}: Props) {
+  return (
+    <>
+      <RefreshButton css={[float.right, m.b(2), m.l(2)]} text />
+      <Text css={[text.xl]}>
+        <FormattedMessage defaultMessage="Oops" />
+      </Text>
+      <Text>
+        <FormattedMessage defaultMessage="Something going wrong." />
+      </Text>
+      <Text size="small">
+        <FormattedMessage defaultMessage="This may be caused by a network problem. If the error persists after refreshing the page, please contact the admin." />
+      </Text>
+    </>
+  );
+}
 function Oops({ error, className }: Props) {
   const ref = useRef<HTMLDivElement>(null);
-  const [width] = useContainerQuery(ref, { width: widthBreakpoints });
+  const [width, height] = useContainerQuery(ref, config);
   console.error(error);
+  let oops;
+
+  if (width === 'xs' || height === 'xs') {
+    oops = <OopsXs error={error} />;
+  } else if (height === 'md' && width === 'md') {
+    oops = <OopsLg error={error} />;
+  } else {
+    oops = <OopsMd error={error} />;
+  }
+
   return (
-    <div css={styles.container} data-width={width} className={className} ref={ref}>
-      <div>
-        {width !== 'xs' && (
-          <div css={styles.title}>
-            <FormattedMessage defaultMessage="Oops" />
-          </div>
-        )}
-        <div>
-          <RefreshButton css={width === 'xs' ? [] : [float.right, m.b(2), m.l(2)]} small text={width !== 'xs'} />
-          <Text>
-            <FormattedMessage defaultMessage="Something going wrong." />
-          </Text>
-          {width !== 'xs' && (
-            <Text size="small">
-              <FormattedMessage defaultMessage="This may be caused by a network problem. If the error persists after refreshing the page, please contact the admin." />
-            </Text>
-          )}
-        </div>
-      </div>
+    <div css={styles.container} data-width={width} data-height={height} className={className} ref={ref}>
+      {oops}
     </div>
   );
 }
