85e75ff68e0bd948d477a5f52e56ea60734fe502
Tachibana Kiyomi <me@yuru.me>
2022-06-30 06:47:44 +0900

    fix: fix type annotation of next configuration

--- next.config.js
+++ next.config.js
@@ -1,47 +1,49 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')();
 const ANALYZE = Boolean(process.env.ANALYZE);
 const BACKEND_URL = process.env.BACKEND_URL || 'https://test.boluo.chat';
 
-module.exports = withMDX({
-  reactStrictMode: true,
-  poweredByHeader: false,
-  trailingSlash: true,
-  swcMinify: false,
-  eslint: {
-    dirs: ['src', 'tests'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${BACKEND_URL}/api/:path*`, // Proxy to Backend
-      },
-    ];
-  },
-  webpack: (config) => {
-    if (ANALYZE) {
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-      const plugin = new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
+module.exports = withMDX(
+  /** @type {import('next').NextConfig} */
+  {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    trailingSlash: true,
+    swcMinify: false,
+    eslint: {
+      dirs: ['src', 'tests'],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${BACKEND_URL}/api/:path*`, // Proxy to Backend
+        },
+      ];
+    },
+    webpack: (config) => {
+      if (ANALYZE) {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+        const plugin = new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+        });
+        config.plugins.push(plugin);
+      }
+
+      // noinspection JSValidateTypes
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
       });
-      config.plugins.push(plugin);
-    }
 
-    // noinspection JSValidateTypes
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+      // `react-intl` without parser
+      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+      // https://github.com/vercel/next.js/issues/30434
+      config.resolve.alias['@formatjs/icu-messageformat-parser'] = '@formatjs/icu-messageformat-parser/no-parser';
 
-    // `react-intl` without parser
-    // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
-    // https://github.com/vercel/next.js/issues/30434
-    config.resolve.alias['@formatjs/icu-messageformat-parser'] = '@formatjs/icu-messageformat-parser/no-parser';
-
-    // avoid extra bundle cost, see https://github.com/reduxjs/react-redux/releases/tag/v8.0.0
-    config.resolve.alias['react-redux'] = 'react-redux/es/next';
-    return config;
-  },
-});
+      // avoid extra bundle cost, see https://github.com/reduxjs/react-redux/releases/tag/v8.0.0
+      config.resolve.alias['react-redux'] = 'react-redux/es/next';
+      return config;
+    },
+  }
+);
