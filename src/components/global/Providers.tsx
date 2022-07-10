c51a17c3c4ac34d03aee97164f00feaa9530dff7
Tachibana Kiyomi <me@yuru.me>
2022-07-10 19:45:39 +0900

    fix: add missing <NotificationList/>

--- src/components/global/Providers.tsx
+++ src/components/global/Providers.tsx
@@ -1,14 +1,15 @@
 import type { FC } from 'react';
+import { useMemo } from 'react';
 import type { ChildrenProps } from '../../helper/props';
 
 import { store } from '../../state/store';
-import { useMemo } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ErrorBoundary } from '../ErrorBoundary';
 import { NextLocaleProvider } from './NextLocaleProvider';
 import { SchemeProvider } from './SchemeProvider';
 import { SWRConfig } from 'swr';
 import type { SwrFallbackProps } from '../../helper/SwrProps';
+import { NotificationList } from './NotificationList';
 
 interface Props extends ChildrenProps, SwrFallbackProps {}
 
@@ -24,7 +25,10 @@ export const Providers: FC<Props> = ({ children, swrFallback = {} }) => {
       <SWRConfig value={swrConfig}>
         <SchemeProvider>
           <ErrorBoundary>
-            <NextLocaleProvider>{children}</NextLocaleProvider>
+            <NextLocaleProvider>
+              {children}
+              <NotificationList />
+            </NextLocaleProvider>
           </ErrorBoundary>
         </SchemeProvider>
       </SWRConfig>
